115ad9921686b008d45f159883996b267a760869
lumi1986 <michael.luethi1986@gmail.com>
2024-05-14 07:35:47 +0200

    feat: add swagger

--- src/Functions/Program.cs
+++ src/Functions/Program.cs
@@ -1,12 +1,35 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.OpenApi;
+using AzureFunctions.Extensions.Swashbuckle.Settings;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddSwashBuckle(opts =>
+        {
+            opts.AddNewtonsoftSupport = true;
+            opts.RoutePrefix = "api";
+            opts.SpecVersion = OpenApiSpecVersion.OpenApi3_0;
+            opts.AddCodeParameter = true;
+            opts.PrependOperationWithRoutePrefix = true;
+            opts.Documents = new[]
+            {
+                new SwaggerDocument
+                {
+                    Name = "v1",
+                    Title = "Mitigation calculator",
+                    Version = "v2"
+                }
+            };
+            opts.Title = "Mitigation calculator";
+        });
     })
     .Build();
 
