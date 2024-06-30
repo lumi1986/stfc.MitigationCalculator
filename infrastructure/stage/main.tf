e4738249aad1cf455a66fdf59ad51f3ba53629c6
lumi1986 <michael.luethi1986@gmail.com>
2024-06-30 19:40:33 +0200

    fix: version

--- infrastructure/stage/main.tf
+++ infrastructure/stage/main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_resource_group" "mitigation_calculator" {
-  name     = "mitigation_calculator_${var.environment_short_name}"
+  name     = "mitigation-calculator-${var.environment_short_name}"
   count    = var.is_vritual ? 0 : 1
   location = data.azurerm_resource_group.default.location
 }
@@ -12,7 +12,7 @@ resource "azurerm_container_app" "mitigation_calculator" {
   name                         = "mitigationcalculator-${var.environment_short_name}"
   count                        = var.is_vritual ? 0 : 1
   container_app_environment_id = var.container_app_environment_id
-  resource_group_name          = azurerm_resource_group.mitigation_calculator[1].id
+  resource_group_name          = azurerm_resource_group.mitigation_calculator[0].id
   revision_mode                = "Single"
 
   template {
